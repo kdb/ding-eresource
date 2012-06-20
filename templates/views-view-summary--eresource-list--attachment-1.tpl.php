5c4560637d2bf99a04ae5831f3c95cea04f9f4a4
Mikkel Hoegh <mikkel@hoegh.org>
2012-06-20 02:59:57 +0200

    Added .active class to active letter.

---
+++ templates/views-view-summary--eresource-list--attachment-1.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file
+ * Override the output for the alphabetical index on the eresource page.
+ *
+ * @ingroup views_templates
+ */
+
+// If we're currently filtering on a letter, add an active class to that
+// letter's link.
+if (!empty($view->original_args[1])) {
+  $letter = drupal_strtoupper($view->original_args[1]); 
+  
+  foreach ($rows as $id => $row) {
+    if ($row->link === $letter) {
+      if (isset($classes[$id])) {
+        $classes[$id] .= ' active';
+      }
+      else {
+        $classes[$id] = 'active';
+      }
+    }
+  }
+}
+?>
+<div class="item-list">
+  <ul class="views-summary">
+  <?php foreach ($rows as $id => $row): ?>
+    <li><a href="<?php print $row->url; ?>"<?php print !empty($classes[$id]) ? ' class="'. $classes[$id] .'"' : ''; ?>><?php print $row->link; ?></a>
+      <?php if (!empty($options['count'])): ?>
+        (<?php print $row->count?>)
+      <?php endif; ?>
+    </li>
+  <?php endforeach; ?>
+  </ul>
+</div>
